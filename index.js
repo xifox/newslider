ff28842517d2df094276a27016522bee99a90767
Juan Facundo Suarez <facundo@svarez.com.ar>
2013-10-16 19:45:48 -0300

    moving files

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 
 /**
- * Components dependency
+ * Component dependencies
  */
 
 var o = require('jquery');
-var NewSlider = require('./newslider');
 
+/**
+* Expose `NewSlider`
+ */
 
+module.exports = NewSlider;
 
-// create a NewSlider instance
-var mySlider = new NewSlider('.my-slider');
+/**
+ * Expose `NewSlider`
+ *
+ * @param {String}jQuery} el
+ * @param {Object} opts
+ *
+ * @api public
+ */
 
-mySlider.next();
+function NewSlider(el){
+  // TODO: uncomment
+  // if (!(this instanceof NewSlider)) return new NewSlider;
 
+  // DOM elements
+  this.el = o(el);
+  this.wrapper = this.el.children();
+
+  // Add custom css classes
+  this.el.addClass('newslider-viewport');
+  this.wrapper.addClass('newslider-wrapper')
+}
+
+/**
+ * Move to next slide
+ */
+
+NewSlider.prototype.next = function(){
+  console.log('move to next');
+  console.log('-> this.el -> ', this.el);
+};
